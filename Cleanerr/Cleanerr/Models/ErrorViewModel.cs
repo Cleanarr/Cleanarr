02de7914905c7b487034ab346bbeccbc33f2de87
Arjen Hissink <appiekap653@hotmail.com>
2020-10-26 12:19:45 +0100

    Project init

---
+++ Cleanerr/Cleanerr/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cleanerr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
