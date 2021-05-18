3c9d388331fac3135cd566d69bb789e39e5a81bb
Arjen Hissink <appiekap653@hotmail.com>
2021-05-18 09:56:50 +0200

    added configuration secrets attribute

---
+++ src/Cleanarr.PluginBase/Attributes/ConfigurationSecretAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Cleanarr.PluginBase.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ConfigurationSecretAttribute : Attribute
+    {
+    }
+}
